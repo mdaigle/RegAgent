5effa58f506a6dc144d64e2eb7c6abcdb8914fc7
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-04-23 21:24:04 -0700

    More agent implementation.

--- server.js
+++ server.js
@@ -1,73 +1,102 @@
 const protocol = require('./protocol');
 // Client States
-const BEGIN = -1
-const REGISTER_WAIT = 0
-const IDLE = 1
-const FETCH_WAIT = 2
-const ACK_WAIT = 3
+// const BEGIN = -1
+// const REGISTER_WAIT = 0
+// const IDLE = 1
+// const FETCH_WAIT = 2
+// const ACK_WAIT = 3
 
-// State Questions:
-// Can a client send a PROBE or FETCH before being registered?
-// In the sequence PROBE (timeout) UNREGISTER, how do you differentiate the ACK
-// responses?
-//
+const args = process.argv.slice(2);
+assert(args.length == 2);
 
-// Commands
-const REGISTER = 1;
-const REGISTERED = 2;
-const FETCH = 3;
-const FETCH_RESPONSE = 4;
-const UNREGISTER = 5;
-const PROBE = 6;
-const ACK = 7;
+var service_name = args[0];
+var service_port = args[1];
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+rl.pause();
+
+const dgram = require('dgram');
+const socket_out = dgram.createSocket('udp4');
+const socket_in = dgram.createSocket('udp4');
+
+socket_out.on('error', (err) => {
+    console.log('Socket error');
+    socket_out.close();
+    socket_in.close();
+    process.exit(1);
+});
+
+socket_in.on('error', (err) => {
+    console.log('Socket error');
+    socket_out.close();
+    socket_in.close();
+    process.exit(1);
+});
+
+var num_listening = 0;
+
+socket_out.on('listening', () => {
+    num_listening++;
+    if (num_listening == 2) {
+        rl.resume();
+    }
+});
+
+socket_in.on('listening', () => {
+    num_listening++;
+    if (num_listening == 2) {
+        rl.resume();
+    }
+});
 
 // Message Handlers //
 function register_callback(msg, rinfo):
   data = protocol.unpackRegistered(msg);
-  // transition to IDLE
 
 
 function fetch_callback(msg, rinfo):
   data = protocol.unpackFetch(msg);
-  // transition to IDLE
 
 function probe_callback(msg, rinfo):
   send_ack();
 
 function ack_callback(msg, rinfo):
-  // transition to IDLE
 
 // Command Handlers //
 function send_register():
   msg = protocol.packRegister(service_addr, service_data, service_name);
   send(msg, function(){
-    // transition to REGISTER_WAIT
   });
 
 function send_fetch():
   msg = protocol.packFetch(service_name);
   send(msg, function(){
-    // transition to FETCH_WAIT
   });
 
 function send_unregister():
   msg = protocol.packUnregister(service_addr);
   send(msg, function(){
-    // transition to ACK_WAIT
   });
 
 function send_probe():
   msg = packProbe(); // NEED TO MAKE THIS FUNCTION
   send(msg, function(){
-    // transition to ACK_WAIT
   });
 
 function send_ack():
   msg = packAck();
   send(msg, function(){
-  
+
   });
 
+rl.on('line', (line) => {
+
+});
 
 
 
@@ -75,7 +104,7 @@ function send_ack():
 // function arguments:
 //    msg
 //    rinfo
-MSG_HANDLER = {"2": register_callback, "4": fetch_callback, "6", probe_callback, "7": ack_callback}; 
+MSG_HANDLER = {"2": register_callback, "4": fetch_callback, "6", probe_callback, "7": ack_callback};
 
 // function arguments:
 //    socket
@@ -108,4 +137,10 @@ socket.on('message', (msg, rinfo) => {
 
 });
 
-// need to bind socket etc.
+
+// IO and IO EVENT BINDINGS
+// -------------------------------------------------------------------------- //
+
+var rand_port = random(2000, 5000);
+socket_out.bind(rand_port);
+socket_in.bind(rand_port + 1);
